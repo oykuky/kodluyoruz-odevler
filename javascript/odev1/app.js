56969447b51efac769742b291178bd3bc67f6b05
Öykü Kaya <oykuky001@gmail.com>
2023-03-25 23:17:41 +0300

    live clock updated

--- javascript/odev1/app.js
+++ javascript/odev1/app.js
@@ -2,3 +2,17 @@ const x=prompt("İsminizi giriniz");
 
 document.getElementById("myName").innerHTML=`${x}`;
 
+
+
+function showTime() {
+    let date = new Date();
+    const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    const day = weekday[date.getDay()];
+    const currentTime = date.getHours() + "." + date.getMinutes() + "." + date.getSeconds();
+    document.getElementById("myClock").innerHTML = `${currentTime} ${day}`;
+  }
+  
+  setInterval(showTime, 1000); // Her saniyede showTime işlevini çağırır
+  
+
+

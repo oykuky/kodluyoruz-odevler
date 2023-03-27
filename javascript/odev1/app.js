4adc29450df26b6edc44b1ba60bd84b00332d153
Öykü Kaya <oykuky001@gmail.com>
2023-03-27 22:26:58 +0300

    updated

--- javascript/odev1/app.js
+++ javascript/odev1/app.js
@@ -6,7 +6,7 @@ function showTime() {
     let date = new Date();
     const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
     const day = weekday[date.getDay()];
-    const currentTime = date.getHours() + ":" + date.getMinutes() + "." + date.getSeconds();
+    const currentTime = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
     document.getElementById("myClock").innerHTML = `${currentTime} ${day}`;
   }
   

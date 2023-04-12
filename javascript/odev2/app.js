3df5552c85c6831936801acc825d338f07e2b1a5
Öykü Kaya <oykuky001@gmail.com>
2023-04-13 00:34:56 +0300

    li ekleme

--- javascript/odev2/app.js
+++ javascript/odev2/app.js
@@ -5,10 +5,12 @@ add.addEventListener('click',newElement)
 function newElement(){
  var liDom=document.createElement('li')
  var UlDom=document.querySelector('#list')
+ var task=document.getElementById('task')
  
- if(){
-    UlDom.append(liDom);
-
+ if(1){
+   liDom.textContent=task.value;
+   UlDom.append(liDom)
+   task.value="";
  }
  else{
  

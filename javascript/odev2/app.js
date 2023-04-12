fba5928594b203638a73e1df129847510efd6e77
Öykü Kaya <oykuky001@gmail.com>
2023-04-13 00:22:44 +0300

    add button eklendi

---
+++ javascript/odev2/app.js
@@ -0,0 +1,25 @@
+//Ekle butonu
+var add = document.querySelector('#liveToastBtn')
+add.addEventListener('click',newElement)
+
+function newElement(){
+ var liDom=document.createElement('li')
+ var UlDom=document.querySelector('#list')
+ 
+ if(){
+    UlDom.append(liDom);
+
+ }
+ else{
+ 
+
+ }
+
+
+
+}
+
+
+function remove(){
+
+}

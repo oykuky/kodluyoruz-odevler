c81e5e3ca3e15bad99dcf3c26db7a26e4ff98b44
Öykü Kaya <oykuky001@gmail.com>
2023-04-15 00:38:21 +0300

    up

--- javascript/odev2/app.js
+++ javascript/odev2/app.js
@@ -1,27 +1,33 @@
 //Ekle butonu
-var add = document.querySelector('#liveToastBtn')
-add.addEventListener('click',newElement)
+function newElement() {
+    var liDom = document.createElement('li')  //yeni bir liste elemanı oluşturduk
+    var UlDom = document.querySelector('#list') //liste etiketini seçtik
+    var task = document.querySelector('#task') //input verilerimizi alacağımız ogeyi seçtik
+    var toastDom = document.querySelector('#liveToast')
 
-function newElement(){
- var liDom=document.createElement('li')
- var UlDom=document.querySelector('#list')
- var task=document.getElementById('task')
- 
- if(1){
-   liDom.textContent=task.value;
-   UlDom.append(liDom)
-   task.value="";
- }
- else{
- 
+    if (task.value.length === 0 || task.value === "") {
+        
 
- }
+    }
+    else {
+
+        liDom.textContent = task.value;  //input task'den gelen verileri li elementine yazdık
+        UlDom.append(liDom)     //Listeye elemanımızı ekledik
+        task.value = "";
+
+    }
+
 
 
 
 }
 
 
-function remove(){
+function remove() {
 
+}
+
+
+function GetToast(){
+    
 }

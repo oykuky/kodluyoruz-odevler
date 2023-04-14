7e170f4e97b482ac8d10b83b700f884025ce173a
Öykü Kaya <oykuky001@gmail.com>
2023-04-15 01:27:34 +0300

    silme kaldı

--- javascript/odev2/app.js
+++ javascript/odev2/app.js
@@ -1,27 +1,34 @@
+const kapat = `<span class="close" onclick="remove(parentNode)" aria-label="Close" aria-hidden="true">&times;</span>`;
+var UlDom = document.querySelector('#list') //liste etiketini seçtik
+var task = document.querySelector('#task') //input verilerimizi alacağımız ogeyi seçtik
+
+//toggle ile sınıfa geçiş yaparız
+function liClicked(){
+    this.classList.toggle("checked")
+}
 //Ekle butonu
 function newElement() {
-    var liDom = document.createElement('li')  //yeni bir liste elemanı oluşturduk
-    var UlDom = document.querySelector('#list') //liste etiketini seçtik
-    var task = document.querySelector('#task') //input verilerimizi alacağımız ogeyi seçtik
-
+   
+   
     if (task.value.length === 0 || task.value === "") {
         $('.error').toast('show');   //burada index dosyamızdaki Toast bölümündeki error class seçilir
     }
     else {
-
-        liDom.textContent = task.value;  //input task'den gelen verileri li elementine yazdık
+        let liDom = document.createElement('li')  //yeni bir liste elemanı oluşturduk
+        // liDom.textContent = task.value;  
+        // UlDom.innerHTML+=`${kapat}`
+        liDom.innerHTML = `${task.value}${kapat}`;
+        liDom.addEventListener("click",liClicked)
         UlDom.append(liDom)     //Listeye elemanımızı ekledik
+
+        $('.success').toast('show');
         task.value = "";
 
     }
-
-
-
-
 }
 
 
-function remove() {
-
+function remove(parentNode) {
+    
 }
 

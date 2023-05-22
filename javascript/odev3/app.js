77238936b317cc441507251f22b85e8a3cc39ac3
Öykü Kaya <oykuky001@gmail.com>
2023-05-22 22:05:03 +0300

    odev3 added

---
+++ javascript/odev3/app.js
@@ -0,0 +1,83 @@
+const menu = [
+  {
+    id: 1,
+    title: "Tteokbokki",
+    category: "Korea",
+    price: 10.99,
+    img:
+      "./img/tteokbokki.jpg",
+    desc: `Spicy rice cakes, serving with fish cake.`,
+  },
+  {
+    id: 2,
+    title: "Chicken Ramen",
+    category: "Japan",
+    price: 7.99,
+    img:
+      "./img/chicken-ramen.jpg",
+    desc: `Chicken noodle soup, serving with vegetables such as soy bean, green onion. In an optional you can ask for egg. `,
+  },
+  {
+    id: 3,
+    title: "Bibimbap",
+    category: "Korea",
+    price: 8.99,
+    img:
+    "./img/bibimbap.jpg",
+    desc: `Boiling vegetables, serving with special hot sauce`,
+  },
+  {
+    id: 4,
+    title: "Dan Dan Mian",
+    category: "China",
+    price: 5.99,
+    img:
+    "./img/Dan-Dan-Noodles-10.jpg",
+    desc: `Dan dan noodle, serving with green onion `,
+  },
+  {
+    id: 5,
+    title: "Yangzhou Fried Rice",
+    category: "China",
+    price: 12.99,
+    img:
+    "./img/Yangzhou-Fried-Rice1.jpg",
+    desc: `Yangzhou style fried rice, serving with bean and pickles `,
+  },
+  {
+    id: 6,
+    title: "Onigiri",
+    category: "Japan",
+    price: 9.99,
+    img:
+      "./img/Onigiri-3-1-of-1.jpg",
+    desc: `Rice Sandwich, serving with soy sauce`,
+  },
+  {
+    id: 7,
+    title: "Jajangmyeon",
+    category: "Korea",
+    price: 15.99,
+    img:
+      "./img/Jajangmyeon-Korean-Noodle.jpg",
+    desc: `Black bean sauce noodle, serving with green onion `,
+  },
+  {
+    id: 8,
+    title: "Ma Yi Shang Shu",
+    category: "China",
+    price: 12.99,
+    img:
+      "./img/MaYi.jpg",
+    desc: `Hot pepper sauce noodle, serving with soy bean and onion`,
+  },
+  {
+    id: 9,
+    title: "Doroyaki",
+    category: "Japan",
+    price: 3.99,
+    img:
+      "./img/Dorayaki.jpg",
+    desc: `Red bean paste dessert, serving with honey.`,
+  },
+];
